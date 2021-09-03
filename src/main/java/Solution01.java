bab8db5db7b77629c13746587df117af282f568e
Brian <Brian.reed1202@gmail.com>
2021-09-03 16:33:53 -0400

    Initial commit

---
+++ src/main/java/Solution01.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution01 {
+    private static final Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("What is your name? ");
+        String name = in.nextLine();
+        System.out.println("Hello, " + name + ", nice to meet you!");
+    }
+}
